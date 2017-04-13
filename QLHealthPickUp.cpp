915f2fb5803e9d48887a1da5f7e2e1f70c757024
Tianyu Liu <kingcrimsontianyu@gmail.com>
2017-04-13 17:41:58 -0400

    Fixed the issue where the health pickup sound was not played.

--- QLHealthPickUp.cpp
+++ QLHealthPickUp.cpp
@@ -29,7 +29,7 @@ AQLHealthPickUp::AQLHealthPickUp()
     BoxComponent->SetCollisionResponseToChannel(ECollisionChannel::ECC_Pawn, ECollisionResponse::ECR_Overlap);
 
     // sound
-    SoundComponentList.Add("Consume", CreateSoundComponent(RootComponent, TEXT("/Game/Sounds/bite"), TEXT("HealthPickUpConsume")));
+    FireAndForgetSoundWaveList.Add("Consume", CreateFireAndForgetSoundWave(TEXT("/Game/Sounds/bite"), TEXT("HealthPickUpConsume")));
 
     // built-in dynamic delegate
     this->OnActorBeginOverlap.AddDynamic(this, &AQLHealthPickUp::OnOverlapBeginForActor);
@@ -58,7 +58,7 @@ void AQLHealthPickUp::OnOverlapBeginForActor(AActor* OverlappedActor, AActor* Ot
     AQLCharacter* character = Cast<AQLCharacter>(OtherActor);
     if(character)
     {
-        PlaySoundComponent("Consume");
+        PlaySoundFireAndForget("Consume", character->GetActorLocation());
         character->IncrementHealth(Increment);
         this->Destroy();
     }
