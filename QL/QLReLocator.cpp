013e4128698726e4b3d1ecd12544ca0296bd0a85
Tianyu Liu <kingcrimsontianyu@gmail.com>
2018-04-18 22:16:13 -0400

    Implemented relocator.

--- QL/QLReLocator.cpp
+++ QL/QLReLocator.cpp
@@ -1,28 +1,66 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "QL.h"
+#include "QLUtility.h"
 #include "QLReLocator.h"
+#include "QLCharacter.h"
+#include "QLGameModeBase.h"
 
-
+//------------------------------------------------------------
 // Sets default values
+//------------------------------------------------------------
 AQLReLocator::AQLReLocator()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+    // built-in dynamic delegate
+    this->OnActorBeginOverlap.AddDynamic(this, &AQLReLocator::OnOverlapBeginForActor);
 }
 
+//------------------------------------------------------------
 // Called when the game starts or when spawned
+//------------------------------------------------------------
 void AQLReLocator::BeginPlay()
 {
 	Super::BeginPlay();
 	
 }
 
+//------------------------------------------------------------
 // Called every frame
+//------------------------------------------------------------
 void AQLReLocator::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
 }
 
+//------------------------------------------------------------
+//------------------------------------------------------------
+void AQLReLocator::OnOverlapBeginForActor(AActor* OverlappedActor, AActor* OtherActor)
+{
+    AQLCharacter* QLCharacter = Cast<AQLCharacter>(OtherActor);
+    if (QLCharacter)
+    {
+        if (GetWorld())
+        {
+            AQLGameModeBase* QLGameModeBase = Cast<AQLGameModeBase>(GetWorld()->GetAuthGameMode());
+            APlayerController* PlayerController = GetWorld()->GetFirstPlayerController();
+            if (QLGameModeBase && PlayerController)
+            {
+                AActor* Result = QLGameModeBase->ChoosePlayerStart(PlayerController);
+                if (Result)
+                {
+                    APlayerStart* PlayerStart = Cast<APlayerStart>(Result);
+                    if (PlayerStart)
+                    {
+                        QLCharacter->SetActorTransform(PlayerStart->GetActorTransform());
+                        QLCharacter->GetMovementComponent()->StopMovementImmediately();
+                        PlaySoundFireAndForget("Relocate");
+                    }                                        
+                }
+            }
+        }
+    }
+}
--- QL/QLReLocator.h
+++ QL/QLReLocator.h
@@ -13,23 +13,38 @@
 #include "QLActor.h"
 #include "QLReLocator.generated.h"
 
+//------------------------------------------------------------
+// Relocate player properly as soon as he overlaps
+// with AQLReLocator
+//------------------------------------------------------------
 UCLASS()
-class QL_API AQLReLocator : public AActor
+class QL_API AQLReLocator : public AQLActor
 {
 	GENERATED_BODY()
 	
-public:	
+public:
+    //------------------------------------------------------------
 	// Sets default values for this actor's properties
+    //------------------------------------------------------------
 	AQLReLocator();
 
 protected:
+    //------------------------------------------------------------
 	// Called when the game starts or when spawned
+    //------------------------------------------------------------
 	virtual void BeginPlay() override;
 
-public:	
+public:
+    //------------------------------------------------------------
 	// Called every frame
+    //------------------------------------------------------------
 	virtual void Tick(float DeltaTime) override;
 
-	
+    //------------------------------------------------------------
+    // Upon overlapping, teleport player immediately and cancel all
+    // movement
+    //------------------------------------------------------------
+    UFUNCTION(BlueprintCallable)
+    void OnOverlapBeginForActor(AActor* OverlappedActor, AActor* OtherActor);
 	
 };
